8ad1f801822128be3880410be74545a010f2169c
Pedro Sérgio <www.psro@gmail.com>
2020-11-29 11:04:25 -0300

    includes fix

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import About from './pages/About';
-import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
