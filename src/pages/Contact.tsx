1f6c593c0f1b2c61ab5577e1be08055e91a4f950
Pedro <36551559+seapvnk@users.noreply.github.com>
2021-02-02 09:14:57 -0300

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,8 +5,8 @@ function Contact() {
         <div className="md doc container">
             <h1>Contato</h1>
             <h2>Informações para contato</h2>
-            <p>📱 Fone/Whatsapp: (41) 9 9896-9364</p>
-            <p>📧 E-mail: www.psro@gmail.com</p>
+            <p>📱 Fone/Whatsapp: (41) 9 9999-9999</p>
+            <p>📧 E-mail: email@gmail.com</p>
             <p>🖥️ Github: <a target="_blank" href="https://github.com/seapvnk">https://github.com/seapvnk</a></p>
         </div>
     )
